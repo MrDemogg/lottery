1a497f3ee211d2ec406b36c1bd16835c6977761d
demog <dg08gg@mail.ru>
2022-07-29 16:40:53 +0300

    фикс бага - кнопка срабатывала без нажатия

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Lottery extends Component {
+  state = {
+    oldNumbers: [],
+    newNumbers: []
+  }
+  randomNumbers = () => {
+    this.setState({
+      oldNumber: [...this.state.oldNumbers, ...this.state.newNumbers]
+    })
+    this.state.newNumbers =  [];
+    for (let i = 0; i < 5; i++) {
+      let number = Math.floor(5 - 0.5 + Math.random() * (36 - 5 + 1));
+      console.log(number)
+      if (this.state.oldNumbers.includes(number)) {
+        i--
+      } else {
+        this.state.newNumbers.push(number)
+      }
+    }
+    console.log(this.state.oldNumbers)
+    console.log(this.state.newNumbers)
+  }
+  render() {
+    return (
+      <div className="lottery">
+        <div className="lottery__button">
+          <button onClick={() => this.randomNumbers()}>REFRESH</button>
+        </div>
+        <div className="lottery__lots">
+          <div className="lottery__lot">{this.state.newNumbers[0]}</div>
+          <div className="lottery__lot">{this.state.newNumbers[1]}</div>
+          <div className="lottery__lot">{this.state.newNumbers[2]}</div>
+          <div className="lottery__lot">{this.state.newNumbers[3]}</div>
+          <div className="lottery__lot">{this.state.newNumbers[4]}</div>
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
+export default Lottery;
--- src/index.js
+++ src/index.js
@@ -1,32 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Lottery from './App';
 import reportWebVitals from './reportWebVitals';
 
-class Lottery extends Component {
-  state = {
-    oldNumbers: [],
-    newNumbers: [],
-    randomNumbers: () => {
-      this.state.oldNumbers = this.state.newNumbers;
-      this.state.newNumbers =  [];
-      for (let i = 0; i < 5; i++) {
-        let number = Math.floor(5 - 0.5 + Math.random() * (36 - 5 + 1));
-        console.log(number)
-        if (this.state.oldNumbers.includes(number)) {
-          i--
-        } else {
-          this.state.newNumbers.push(number)
-        }
-      }
-    }
-  }
-}
-const lot = new Lottery;
-lot.state.randomNumbers();
-console.log(lot.state.newNumbers)
-console.log(lot.state.oldNumbers)
-lot.state.randomNumbers();
-console.log(lot.state.newNumbers)
-console.log(lot.state.oldNumbers)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Lottery />
+  </React.StrictMode>
+);
+
+
 reportWebVitals();
