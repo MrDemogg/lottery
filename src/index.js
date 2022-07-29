cc9ff178ba9bcce3e7a92a653ae25ff4c4571053
demog <dg08gg@mail.ru>
2022-07-29 12:48:18 +0300

    система рандомных чисел и удаление использованных

--- src/index.js
+++ src/index.js
@@ -3,14 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+class Lottery extends Component {
+  state = {
+    oldNumbers: [],
+    newNumbers: [],
+    randomNumbers: () => {
+      this.state.oldNumbers = this.state.newNumbers;
+      this.state.newNumbers =  [];
+      for (let i = 0; i < 5; i++) {
+        let number = Math.floor(5 - 0.5 + Math.random() * (36 - 5 + 1));
+        console.log(number)
+        if (this.state.oldNumbers.includes(number)) {
+          i--
+        } else {
+          this.state.newNumbers.push(number)
+        }
+      }
+    }
+  }
+}
+const lot = new Lottery;
+lot.state.randomNumbers();
+console.log(lot.state.newNumbers)
+console.log(lot.state.oldNumbers)
+lot.state.randomNumbers();
+console.log(lot.state.newNumbers)
+console.log(lot.state.oldNumbers)
 reportWebVitals();
